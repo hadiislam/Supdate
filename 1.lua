d95fc8b575168cdf89f3abf83563894899519ca6
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2021-10-28 16:58:53 +0600

    Create 1.lua

---
+++ 1.lua
@@ -0,0 +1,61 @@
+function Main()
+bps = gg.alert(
+[[🎉 ɴᴇᴡ ᴜᴘᴅᴀᴛᴇ ᴀʀʀɪᴠᴇꜱ 🎉
+ᴘʟᴇᴀꜱᴇ ᴜᴘᴅᴀᴛᴇ ᴛʜᴇ ꜱᴄʀɪᴘᴛ 🔥
+
+🔧ʜᴏᴡ ᴛᴏ ᴜᴘᴅᴀᴛᴇ :
+1. ᴛᴀᴘ '🅄🄿🄳🄰🅃🄴' ʙᴜᴛᴛᴏɴ
+2. ᴘᴀꜱᴛᴇ ʟɪɴᴋ ᴛᴏ ᴀɴʏ ʙʀᴏᴡꜱᴇʀ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴇᴡ ᴏɴᴇ.
+
+⭕ɪꜰ ʏᴏᴜ ɢᴇᴛ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ, ꜱᴇɴᴅ ꜰᴇᴇᴅʙᴀᴄᴋ.
+]] , ("🄵🄴🄴🄳🄱🄰🄲🄺"),("🅄🄿🄳🄰🅃🄴"))
+
+if bps == nil then
+  else
+    if bps == 1 then
+      c1()
+    end
+    if bps == 1 then
+      c2()
+    end
+  end
+  Hadi = -1
+  os.exit()
+end
+
+function c1()
+gg.copyText("https://t.me/hadi_islam")
+end
+Main()
+function Main()
+bps = gg.alert(
+[[🎉 ɴᴇᴡ ᴜᴘᴅᴀᴛᴇ ᴀʀʀɪᴠᴇꜱ 🎉
+ᴘʟᴇᴀꜱᴇ ᴜᴘᴅᴀᴛᴇ ᴛʜᴇ ꜱᴄʀɪᴘᴛ 🔥
+
+🔧ʜᴏᴡ ᴛᴏ ᴜᴘᴅᴀᴛᴇ :
+1. ᴛᴀᴘ 🅄🄿🄳🄰🅃🄴 ʙᴜᴛᴛᴏɴ
+2. ᴘᴀꜱᴛᴇ ʟɪɴᴋ ᴛᴏ ᴀɴʏ ʙʀᴏᴡꜱᴇʀ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴇᴡ ᴏɴᴇ.
+
+⭕ɪꜰ ʏᴏᴜ ɢᴇᴛ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ, ꜱᴇɴᴅ ꜰᴇᴇᴅʙᴀᴄᴋ.
+]] , ("🄵🄴🄴🄳🄱🄰🄲🄺"),("🅄🄿🄳🄰🅃🄴"))
+
+if bps == nil then
+  else
+    if bps == 1 then
+      c1()
+    end
+    if bps == 2 then
+      c2()
+    end
+  end
+  Hadi = -1
+  os.exit()
+end
+
+function c1()
+gg.copyText("https://t.me/hadi_islam")
+end
+function c2()
+gg.copyText("https://t.me/hadi_islam")
+end
+Main()
