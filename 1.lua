ace87abf6b2928ace3887f07f91d97d991ab7169
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2021-10-28 21:45:54 +0600

    Delete 1.lua

--- 1.lua
+++
@@ -1,9 +0,0 @@
-local﻿ file = gg.makeRequest('https://raw.githubusercontent.com/hadiislam/Supdate/main/off.lua')
-if file.content == nil or file.content == '' then
-os.exit()
-end﻿
-local load =  load(file.content)
-if load == nil﻿﻿ then
-os.exit()
-end 
-load()
